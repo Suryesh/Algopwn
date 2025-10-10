d3dc6798a185be728c30d29d1770f69eab11f68f
SURYESH <88741760+Suryesh@users.noreply.github.com>
2025-10-10 18:36:41 +0530

    Update algopwn.py

--- algopwn.py
+++ algopwn.py
@@ -6,7 +6,7 @@ import re
 from colorama import Fore, Style, init
 
 init(autoreset=True)
-SCRIPT_VERSION = "1.0.5"
+SCRIPT_VERSION = "1.0.6"
 REMOTE_SCRIPT_URL = "https://raw.githubusercontent.com/Suryesh/Algopwn/main/algopwn.py"
 
 
@@ -102,7 +102,7 @@ def check_for_updates():
 # acl permission
 
 SENSITIVE_ACLS = {
-    "addObject", "deleteObject", "deleteIndex", "editSettings", "addUserKey", "deleteUserKey", "editSettings","seeUnretrievableAttributes","analytics","logs",
+    "addObject", "deleteObject", "deleteIndex", "editSettings", "addUserKey", "deleteUserKey", "editSettings","seeUnretrievableAttributes","logs",
 }
 INFORMATIVE_ACLS = {"search", "listIndexes", "settings"}
 
